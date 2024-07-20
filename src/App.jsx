c67fa8b9dd74cf8aea591ce1d5833f2d2f5e8e21
jordan <andersonjor@deakin.edu.au>
2024-07-20 16:37:18 +1000

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import NewPost from "./NewPost";
 
 function App() {
+  const [submitted, setSubmitted] = useState(true);
   return (
     <div className="bg-emerald-900 w-screen h-screen flex items-center justify-center">
-      <NewPost />
+      {!submitted && <NewPost />}
+      {submitted && <>}
     </div>
   );
 }
